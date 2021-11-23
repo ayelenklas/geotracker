cb96d09554e4f26d051c12501ffbd0c71c8c1d80
Nicole Cristine Dressler <nicole.c.dressler@gmail.com>
2021-11-23 17:48:29 +0100

    trying to make it work

--- geotracker/scraping/scraping_foodpanda.py
+++ geotracker/scraping/scraping_foodpanda.py
@@ -7,6 +7,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+driver = webdriver.Chrome(chrome_options=options,
+                          executable_path=r'C:\path\to\chromedriver.exe')
+driver.get('http://google.com/')
+
+
 options = Options()
 options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
