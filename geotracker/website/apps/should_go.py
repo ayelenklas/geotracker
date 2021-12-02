94c3b6ada11920e3dee50963ac45896bedc6d9b7
Malte Berneaud <malte.berneaud@gmail.com>
2021-12-02 16:48:29 +0100

    removed duplicates part 3

--- geotracker/website/apps/should_go.py
+++ geotracker/website/apps/should_go.py
@@ -118,7 +118,7 @@ def app():
         df["address"] = df["address"].str.replace("\.0", "")
         df = df.assign(hack='').set_index('hack')
         df.columns = ["Name", "Address", "Type of Cuisine"]
-        df.drop_duplicates(subset=["Name", "Address", "Cuisine"])
+        df.drop_duplicates(subset=["Name", "Address"])
 
         return df
 
